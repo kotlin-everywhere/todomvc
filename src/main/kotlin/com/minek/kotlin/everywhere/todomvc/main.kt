926c814bbdb4e9e168d0fd1a9a14427e226fc962
Park Sung Min <vamf12@gmail.com>
2017-09-06 16:16:06 +0900

    시작 지점 기본 스타일 추가

--- src/main/kotlin/com/minek/kotlin/everywhere/todomvc/main.kt
+++ src/main/kotlin/com/minek/kotlin/everywhere/todomvc/main.kt
@@ -4,8 +4,19 @@ import com.minek.kotlin.everywhere.keuix.browser.html.Html
 import com.minek.kotlin.everywhere.keuix.browser.runBeginnerProgram
 import org.w3c.dom.Element
 
+class Model
+class Msg
+
+fun update(msg: Msg, model: Model): Model {
+    return model
+}
+
+fun view(model: Model): Html<Msg> {
+    return Html.text("Hello, Todo MVC")
+}
+
 @Suppress("unused")
 @JsName("main")
 fun main(container: Element) {
-    runBeginnerProgram(container, Html.text("Hello, Todo MVC"))
+    runBeginnerProgram(container, Model(), ::update, ::view)
 }
