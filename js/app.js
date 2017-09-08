bde90d928716e707be0538294e74bc7f74d60589
Park Sung Min <vamf12@gmail.com>
2017-09-08 20:58:48 +0900

    hmr 적용

--- js/app.js
+++ js/app.js
@@ -5,5 +5,25 @@ require('todomvc-app-css/index.css');
 require('../css/app.css');
 
 var kt = require('todomvc');
-kt.com.minek.kotlin.everywhere.todomvc.main(document.querySelector('#app'));
+var model = null;
+if (module.hot) {
+    module.hot.accept();
 
+    module.hot.dispose(function (data) {
+        data.appModel = program.stop();
+        while (app.hasChildNodes()) {
+            app.removeChild(app.firstChild);
+        }
+    });
+
+    if (module.hot.data) {
+        if (module.hot.data.appModel) {
+            model = module.hot.data.appModel;
+        }
+    }
+}
+
+var app = document.querySelector('#app');
+var root = document.createElement('div');
+app.appendChild(root);
+var program = kt.com.minek.kotlin.everywhere.todomvc.main(root, model);
--- src/main/kotlin/com/minek/kotlin/everywhere/todomvc/main.kt
+++ src/main/kotlin/com/minek/kotlin/everywhere/todomvc/main.kt
@@ -1,5 +1,6 @@
 package com.minek.kotlin.everywhere.todomvc
 
+import com.minek.kotlin.everywhere.keuix.browser.Program
 import com.minek.kotlin.everywhere.keuix.browser.html.Html
 import com.minek.kotlin.everywhere.keuix.browser.runBeginnerProgram
 import org.w3c.dom.Element
@@ -17,6 +18,6 @@ fun view(model: Model): Html<Msg> {
 
 @Suppress("unused")
 @JsName("main")
-fun main(container: Element) {
-    runBeginnerProgram(container, Model(), ::update, ::view)
+fun main(container: Element, model: Model?): Program<Model, Msg> {
+    return runBeginnerProgram(container, model ?: Model(), ::update, ::view)
 }
